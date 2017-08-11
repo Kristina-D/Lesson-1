56ffb641075dccc722c609bca7d04e8abf3c8936
Kristina <30863912+Kristina-D@users.noreply.github.com>
2017-08-11 22:54:07 +0200

    Add files via upload

---
+++ Lesson1-Task2.rb
@@ -0,0 +1,7 @@
+#Lesson1-Task2 Triangle
+print "Enter the base of triangle: "
+triangle_base = Integer(gets.chomp)
+print "Enter the height of triangle: "
+triangle_height = Integer(gets.chomp)
+triangle_area = triangle_base*triangle_height/2
+puts "The triangle area is #{triangle_area}"
