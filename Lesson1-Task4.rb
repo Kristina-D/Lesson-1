51b5abd92b174c326821f32d0f06355b87451506
Kristina <30863912+Kristina-D@users.noreply.github.com>
2017-08-16 14:08:15 +0200

    Update Lesson1-Task4.rb

--- Lesson1-Task4.rb
+++ Lesson1-Task4.rb
@@ -1,18 +1,21 @@
 #Lesson1-Task4 Quadratic equation
 puts "Expression: ах^2 + bx + c = 0"
 print "Enter the first coeffecient 'a': "
-a = Integer(gets.chomp)
-print "Enter the second coeffecient 'b': "
-b = Integer(gets.chomp)
-print "Enter the third coeffecient c: "
-c = Integer(gets.chomp)
-D = (b**2 - 4*a*c)
+a = gets.chomp.to_f
 
-if D > 0
-	C = Math.sqrt(D)
-	puts " Discriminant  = #{D}, x1 = #{(-b + C)/(2*a)}, x2 = #{(-b - C)/(2*a)}"
-elsif D == 0
-	puts " Discriminant  = #{D}, x1 = #{-b/(2*a)}"
-elsif D < 0
-	puts "`Discriminant  = #{D}. No roots."
+print "Enter the second coeffecient 'b': "
+b = gets.chomp.to_f
+
+print "Enter the third coeffecient c: "
+c = gets.chomp.to_f
+puts a, b,  c
+discriminant = b**2 - 4*a*c
+
+if discriminant > 0
+  c_sqrt = Math.sqrt(discriminant)
+  puts " Discriminant  = #{discriminant}, x1 = #{(-b + c_sqrt)/(2*a)}, x2 = #{(-b - c_sqrt)/(2*a)}"
+elsif discriminant == 0
+  puts " Discriminant  = #{discriminant}, x1 = #{-b/(2*a)}"
+elsif discriminant < 0
+  puts " Discriminant  = #{discriminant}. No roots."
 end
