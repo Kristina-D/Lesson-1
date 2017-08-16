c41b130a9c29f095cfb8d1e4b7d3ea34a45f357f
Kristina <30863912+Kristina-D@users.noreply.github.com>
2017-08-16 17:40:49 +0200

    Update Lesson1-Task3.rb

--- Lesson1-Task3.rb
+++ Lesson1-Task3.rb
@@ -1,18 +1,19 @@
 #Lesson1-Task3 Right triangle
 print "Enter the 1st side of the triangle: "
-side_a = Integer(gets.chomp)
+side_a = gets.chomp.to_f
 print "Enter the 2nd side of the triangle: "
-side_b = Integer(gets.chomp)
+side_b = gets.chomp.to_f
 print "Enter the 3rd side of the triangle: "
-side_c = Integer(gets.chomp)
+side_c = gets.chomp.to_f
 
-if (side_a**2 + side_b**2 == side_c**2)||(side_a**2+side_c**2 == side_b**2)||(side_c**2 + side_b**2 == side_a**2)
-	puts "The triangle is rectangular. "
+gipotenuza = [side_a, side_b, side_c].sort
+
+if gipotenuza[2]**2 == gipotenuza[0]**2 + gipotenuza[1]**2
+  puts "The triangle is rectangular. "
 end
 
 if (side_a == side_b) && (side_b == side_c)
-    puts "This is an equilateral triangle."
+  puts "This is an equilateral triangle."
 elsif (side_a == side_b)||(side_b ==side_c)||(side_a == side_c)
-	puts "This is an isosceles triangle."
+  puts "This is an isosceles triangle."
 end
-
